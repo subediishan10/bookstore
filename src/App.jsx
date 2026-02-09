e9a76e06e51bb0b3dc7b3ea0c78fa56e8ce85883
Ishan Subedi <subediishan10@gmail.com>
2026-02-09 09:00:43 -0800

    navbar updated

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Banner from "./components/Banner";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+    </>
+  );
+}
+
+export default App;
